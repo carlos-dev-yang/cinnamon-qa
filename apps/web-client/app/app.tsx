30e4c2dbf2d693632ccb4f39624ba6267d85352d
carlos-yang-mnetplus <carlos-yang@mnetplus.world>
2025-07-01 23:50:48 +0900

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/j7LboKWLQ5

---
+++ apps/web-client/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@cinnamon-qa/web-client" />
+    </div>
+  );
+}
+
+export default App;
